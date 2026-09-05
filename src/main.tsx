e2e9978fbf01b6921bfdc0c28299a4d2a3376ace
Michael Kubit <mdkubit@gmail.com>
2026-09-05 19:47:15 -0400

    feat: enable browser routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
-import UniversalHorizonLanding from './site/UniversalHorizonLanding'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UniversalHorizonLanding />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
